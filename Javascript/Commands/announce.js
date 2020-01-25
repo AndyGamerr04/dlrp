1c88aaebc23310a57938d57fa298e41048375c5d
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-01-25 18:46:25 +0100

    Update announce.js

--- Javascript/Commands/announce.js
+++ Javascript/Commands/announce.js
@@ -7,22 +7,17 @@ module.exports.run = async (bot, message, args) => {
 
     var announce = args.join(" ");
 
-    var user = message.author;
-
     if (!announce) return message.channel.send("!announce (announce + link)");
 
     var announceEmbed = new discord.RichEmbed()
         .setColor("#ff9900")
-        .setDescription(`**Dutch Life Roleplay Announcement | ${user}**\n\n${announce}`)
+        .setDescription(`**Dutch Life Roleplay Announcement | @everyone **\n\n${announce}`)
         .setFooter(`Dutch Life Roleplay | ${moment.utc(message.createdAt).format("DD MMM YYYY, HH:mm:ss")}`)
 
     var announceChannel = message.guild.channels.find(`name`, "📍announcements");
     if (!announceChannel) return message.guild.send("Kan het kanaal niet vinden");
 
-    announceChannel.send(announceEmbed).then(embedMessage => {
-        embedMessage.react('👍');
-        embedMessage.react('👎');
-    });
+    announceChannel.send(announceEmbed);
 
 }
 

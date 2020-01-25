a5bb06f642ee188c664ee7761c4c790647272640
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-01-25 18:47:38 +0100

    Update announce.js

--- Javascript/Commands/announce.js
+++ Javascript/Commands/announce.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
     if (!announce) return message.channel.send("!announce (announce + link)");
 
     var announceEmbed = new discord.RichEmbed()
-        .setColor("#ff9900")
+        .setColor("#009eff")
         .setDescription(`**Dutch Life Roleplay Announcement | @everyone **\n\n${announce}`)
         .setFooter(`Dutch Life Roleplay | ${moment.utc(message.createdAt).format("DD MMM YYYY, HH:mm:ss")}`)
 

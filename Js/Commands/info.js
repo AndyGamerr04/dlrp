9eac7948359d6b8f8d27e7282921fea697ebd43a
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-02-08 16:54:56 +0100

    Update info.js

--- Js/Commands/info.js
+++ Js/Commands/info.js
@@ -44,6 +44,7 @@ module.exports.run = async (bot, message, args) => {
     )
     .addField("Channels", message.guild.channels.size, true)
     .addField("Total Roles", message.guild.roles.size, true)
+    .setThumbnail(guild.icon)
     .addField("Bot Creator", `ΛΝDY#2020`, true);
   return message.channel.send(serverEmbed).then(msg => msg.delete(90000));
 };

30b8bcfbb0eabbecb3cf7d912abaf507a19de985
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-02-08 12:58:09 +0100

    test

--- index.js
+++ index.js
@@ -65,15 +65,11 @@ bot.on("message", async message => {
   if (command) commands.run(bot, message, arguments);
 
   try {
-    null.f()
+    null.arguments()
   } catch (e) {
     console.log(e instanceof TypeError)  // true
     console.log(e.message)               // "null has no properties"
     console.log(e.name)                  // "TypeError"
-    console.log(e.fileName)              // "Scratchpad/1"
-    console.log(e.lineNumber)            // 2
-    console.log(e.columnNumber)          // 2
-    console.log(e.stack)                 // "@Scratchpad/2:2:3\n"
   }
 
 });

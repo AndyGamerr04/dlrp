fe3c4eeef21d6ac0088c00d2e8ef133636c35cab
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-02-09 15:39:20 +0100

    init

--- Js/Commands/commands.js
+++ Js/Commands/commands.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
 
     var serverEmbed = new discord.RichEmbed()
         .setColor(mainColor)
-        .addField("**Command's**", `!8ball\n!avatart\n!hond\n!info\n!kat\n!love\n8me\n!vosje\n\n\n!announce\n!clear\n!idee\n!report\n`)
+        .addField("**Command's**", `!8ball\n!avatart\n!hond\n!info\n!kat\n!love\n!me\n!vosje\n\n\n!announce\n!clear\n!idee\n!report\n`)
     return message.channel.send(serverEmbed).then(msg => msg.delete(99000));
 };
 
--- Js/Commands/me.js
+++ Js/Commands/me.js
@@ -3,6 +3,8 @@ const moment = require("moment");
 const botConfig = require("../../botconfig.json");
 var mainColor = botConfig.mainColor;
 
+var nl = newYork.clone().tz("Europe/Amsterdam");
+
 module.exports.run = async (bot, message, args) => {
 
     const user = message.mentions.users.first() || message.author;
@@ -13,8 +15,8 @@ module.exports.run = async (bot, message, args) => {
         .setThumbnail(user.avatarURL)
         .setColor(mainColor)
         .setAuthor(`${user.username}#${user.discriminator}`, `${user.avatarURL}`)
-        .addField("**Joind Server At**", `${moment.utc(message.member.joinedAt).format("DD MMM YYYY, HH:mm:ss")}`, true)
-        .addField("**Joind Discord At**", `${moment.utc(user.createdAt).format("DD MMM YYYY, HH:mm:ss")}`, true)
+        .addField("**Joind Server At**", `${moment.utc(message.member.joinedAt).tz(nl).format("DD MMM YYYY, HH:mm:ss")}`, true)
+        .addField("**Joind Discord At**", `${moment.utc(user.createdAt).tz(nl).format("DD MMM YYYY, HH:mm:ss")}`, true)
         .setTimestamp()
         .setFooter(`User ID: ${user.id}`);
     return message.channel.send(botEmbed).then(msg => msg.delete(80000));

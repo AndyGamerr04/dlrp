3314fd58a63f5aea481e6a25ae4f970d8f51def1
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-02-24 00:02:42 +0100

    Update idee.js

--- Commands/Commands/idee.js
+++ Commands/Commands/idee.js
@@ -10,12 +10,16 @@ module.exports.run = async (bot, message, args) => {
 
     message.delete();
 
-    if (!message.member.roles.find(r => r.name === "Tester")) {
+    /*if (!message.member.roles.find(r => r.name === "Tester")) {
         var err = new discord.RichEmbed()
             .setColor(errColor)
             .addField(`${errMessage}`, "*You don't have permission.*");
         return message.channel.send(err).then(msg => msg.delete(8000));
-    }
+    }*/
+
+    var idee = args.join(" ");
+    var servername = message.guild.name;
+    var user = message.author;
 
     if (!idee) {
         var ideeErr = new discord.RichEmbed()
@@ -23,11 +27,6 @@ module.exports.run = async (bot, message, args) => {
             .addField(`${errMessage}`, "*You didn't say your idea.*");
         return message.channel.send(ideeErr);
     }
-
-    var idee = args.join(" ");
-    var servername = message.guild.name;
-    var user = message.author;
-
 
     var ideeEmbed = new discord.RichEmbed()
         .setColor(mainColor)
@@ -42,8 +41,8 @@ module.exports.run = async (bot, message, args) => {
 
 }
 
-//var ideeChannel = message.guild.channels.find(`name`, "💡idee-aanvraag");
-//if (!ideeChannel) return message.guild.send("Kan het kanaal niet vinden");
+/*var ideeChannel = message.guild.channels.find(`name`, "💡idee-aanvraag");
+if (!ideeChannel) return message.guild.send("Kan het kanaal niet vinden");*/
 
 module.exports.help = {
     name: "idea"

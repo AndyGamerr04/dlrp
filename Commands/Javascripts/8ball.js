c5e425153861d3e45aa4988a17c146e935311acc
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-04-13 16:38:15 +0200

    reinit

---
+++ Commands/Javascripts/8ball.js
@@ -0,0 +1,42 @@
+const discord = module.require('discord.js');
+
+var fortunes = ["Ja!", "Nee!", "Kan zijn!"];
+
+module.exports.run = async (bot, message, args) => {
+
+  if (!args[0]) {
+
+    var err = new discord.RichEmbed()
+
+      .setColor("2C2F33")
+      .addField(`**Er is iets fout gegaan! 😕**`, "Voer een vraag in die u wilt beantwoorden.\n!8ball + bericht");
+
+    return message.channel.send(err).then(msg => msg.delete(7000));
+
+  }
+
+  if (args[0]) {
+
+    var ball = new discord.RichEmbed()
+
+      .setColor("45bb8a")
+      .setAuthor(fortunes[Math.floor(Math.random() * fortunes.length)]);
+
+    message.channel.send(ball)
+
+  } else {
+
+    var errb = new Discord.RichEmbed()
+
+      .setColor("2C2F33")
+      .addField(`**Er is iets fout gegaan! 😕**`, "Ik kon dat niet lezen. Probeer opnieuw!");
+
+    message.channel.send(errb).then(msg => msg.delete(7000));
+
+  }
+
+}
+
+module.exports.help = {
+  name: "8ball"
+}

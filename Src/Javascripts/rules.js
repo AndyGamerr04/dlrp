a9bac0f2e702de1a585645461466bf586569bec9
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-04-19 19:38:35 +0200

    Create rules.js

---
+++ Src/Javascripts/rules.js
@@ -0,0 +1,39 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    message.delete();
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+
+        var err = new discord.RichEmbed()
+
+            .setColor("2C2F33")
+            .addField("**Er is iets fout gegaan! 😕**", "*Je hebt geen toestemming*");
+
+        return message.channel.send(err).then(msg => msg.delete(7000));
+    }
+
+    var embed = new discord.RichEmbed()
+
+        .setColor("45bb8a")
+
+        .setDescription(`\nrules\n`)
+
+    message.channel.send(embed)
+
+
+    message.channel.send(embed).then(message => {
+
+        message.react('698550597414289499')
+
+            .then(() => message.react('698550597888245770'))
+            .catch(() => console.error('Een van de emoji reageerde niet.'));
+
+    });
+
+}
+
+module.exports.help = {
+    name: "rules"
+}

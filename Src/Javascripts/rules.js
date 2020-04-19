fb71531d8502bfd96f7d928b59f610ff1608afbb
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-04-20 01:49:27 +0200

    Update rules.js

--- Src/Javascripts/rules.js
+++ Src/Javascripts/rules.js
@@ -33,7 +33,7 @@ module.exports.run = async (bot, message, args) => {
 
         .setColor("#de3121")
 
-        .setDescription('**Zwaar wapen**\nGeen zwaar wapen Wapen uittrekken als iemand je gewapend in de gaten houdt.\n\n**Geen RDM**\n(Random Death Match) Iemand zonder reden vermoorden.\n\n**Geen VDM**\n(Random Vehicle Deathmatch) Iemand dood rijden met een auto.\n\n**Geen SpawnKill**\nIemand vermoorden op het moment dat ze verbinden.\n\n**Geen FreeShot**\nIemand zonder reden schieten.')
+        .setDescription('**Geen RDM**\n(Random Death Match) Iemand zonder reden vermoorden.\n\n**Geen VDM**\n(Random Vehicle Deathmatch) Iemand dood rijden met een auto.\n\n**Geen SpawnKill**\nIemand vermoorden op het moment dat ze verbinden.\n\n**Geen FreeShot**\nIemand zonder reden schieten.\n\n**Zwaar wapen**\nGeen zwaar wapen Wapen uittrekken als iemand je gewapend in de gaten houdt.')
 
     message.channel.send(embed2)
 

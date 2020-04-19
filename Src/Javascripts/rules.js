06af2b3092a29f9c00c56ddfe0c4f014f64f42d4
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-04-20 01:39:11 +0200

    Update rules.js

--- Src/Javascripts/rules.js
+++ Src/Javascripts/rules.js
@@ -19,19 +19,50 @@ module.exports.run = async (bot, message, args) => {
 
         .setColor("#00ff11")
 
-        //.setAuthor('Het overtreden van regels leid tot staffzaak')
-
         .setTitle('HometownRP Reglement')
 
         .setURL('https://discord.gg/q8fB9QF')
 
         .setThumbnail('https://i.imgur.com/IBpr3Ow.png')
 
-        .setDescription('**Zwaar wapen**\nGeen zwaar wapen uittrekken als iemand je gewapend in de gaten houdt.\n\n**Geen RDM**\n(Random Death Match) Iemand zonder reden vermoorden.\n\n**Geen VDM**\n(Random Vehicle Deathmatch) Iemand dood rijden met een auto.\n\n**Geen SpawnKill**\nIemand vermoorden op het moment dat ze verbinden.\n\n**Geen FreeShot**\nIemand zonder reden schieten.')
+    message.channel.send(embed)
+
+    var embed2 = new discord.RichEmbed()
+
+        .setColor("#00ff11")
+
+        .setDescription('**Zwaar wapen**\nWapen uittrekken als iemand je gewapend in de gaten houdt.\n\n**Geen RDM**\n(Random Death Match) Iemand zonder reden vermoorden.\n\n**Geen VDM**\n(Random Vehicle Deathmatch) Iemand dood rijden met een auto.\n\n**Geen SpawnKill**\nIemand vermoorden op het moment dat ze verbinden.\n\n**Geen FreeShot**\nIemand zonder reden schieten.')
+
+    message.channel.send(embed2)
+
+
+    var embed3 = new discord.RichEmbed()
+
+        .setColor("#00ff11")
+
+        .setDescription('**Zwaar wapen**\nWapen uittrekken als iemand je gewapend in de gaten houdt.\n\n**Geen RDM**\n(Random Death Match) Iemand zonder reden vermoorden.\n\n**Geen VDM**\n(Random Vehicle Deathmatch) Iemand dood rijden met een auto.\n\n**Geen SpawnKill**\nIemand vermoorden op het moment dat ze verbinden.\n\n**Geen FreeShot**\nIemand zonder reden schieten.')
+
+    message.channel.send(embed3)
+
+
+    /*var embed4 = new discord.RichEmbed()
+
+        .setColor("#00ff11")
+
+        //.setAuthor('Het overtreden van regels leid tot staffzaak')
+
+        //.setTitle('HometownRP Reglement')
+
+        //.setURL('https://discord.gg/q8fB9QF')
+
+        .setThumbnail('https://i.imgur.com/IBpr3Ow.png')
+
+        .setDescription('**Zwaar wapen**\nWapen uittrekken als iemand je gewapend in de gaten houdt.\n\n**Geen RDM**\n(Random Death Match) Iemand zonder reden vermoorden.\n\n**Geen VDM**\n(Random Vehicle Deathmatch) Iemand dood rijden met een auto.\n\n**Geen SpawnKill**\nIemand vermoorden op het moment dat ze verbinden.\n\n**Geen FreeShot**\nIemand zonder reden schieten.')
 
         .setFooter('Copyright HometownRP 2020 ©');
 
-    message.channel.send(embed)
+    message.channel.send(embed4)*/
+
 
 }
 

c3145b62efbd0c4baf3fb62c3eaf5ee1a33e2c75
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-04-19 20:03:02 +0200

    Update rules.js

--- Src/Javascripts/rules.js
+++ Src/Javascripts/rules.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, member, args) => {
 
     message.delete();
 
-    var role = member.guild.role.find('name', 'Agree')
+    var role = message.guild.roles.find('name', 'Agree')
 
     if (!message.member.hasPermission("MANAGE_MESSAGES")) {
 

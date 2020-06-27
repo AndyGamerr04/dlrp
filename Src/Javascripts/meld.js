cb7e13b403087c3e802a34fc86154530df88e63b
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-06-27 19:07:25 +0200

    Update meld.js

--- Src/Javascripts/meld.js
+++ Src/Javascripts/meld.js
@@ -16,28 +16,49 @@ module.exports.run = async(bot, message, args) => {
 
     const user = message.mentions.users.first() || message.author;
 
-    var announce = args.join(" ");
-
+    //var announce = args.join(" ");
     //if (!announce) return message.channel.send("!meld (bericht)");
-    if (!args[0]) {
+    /*if (!args[0]) {
 
         var err2 = new discord.RichEmbed()
 
         .setColor("2C2F33")
             .addField(`**Er is iets fout gegaan! 😕**`, "*Geen* **Mededeling** *meegegeven, gelieve een idee mee te geven.*  |  !meld + bericht");
         return message.channel.send(err2).then(msg => msg.delete(7000));
-    }
+    }*/
 
     var announceEmbed = new discord.RichEmbed()
 
+    var splitser = "//";
+
+    if (args[0] == null) {
+
+        var useMesagge = new discord.RichEmbed()
+
+        .setColor("2C2F33")
+            .setDescription(`!meld **Bericht** ${splitser} **Image-URL**`)
+            .addField(`**Er is iets fout gegaan! 😕**`, "*Geen* **Mededeling** *meegegeven, gelieve een idee mee te geven.*  |  !meld + bericht");
+
+        return message.channel.send(useMesagge).then(msg => msg.delete(25000));
+
+    }
+
+    args = args.join(" ").split(splitser);
+
+    var options = {
+
+        bericht: args[0] || "Geen inhoud gespecificeerd",
+        link: args[1] || ""
+
+    }
+
     .setColor("005fff")
         .setAuthor('Mededeling Hometown Roleplay')
-        //.setDescription(`\n\n${announce}\n\n`)
-        .addField('--------------------------------------------------------------------', '\n\n${announce}\n\n')
-
-    //.setFooter('Hometown Roleplay V2 2020', );
-    //.setFooter(`Mededeling van ${user.username}#${user.discriminator}`);
-    //'https://i.imgur.com/rNz0Wwj.png'
+        .setDescription(`\n\n${options.bericht}\n\n`)
+        .setImage(`${options.link}`)
+        //.setFooter('Hometown Roleplay V2 2020', );
+        //.setFooter(`Mededeling van ${user.username}#${user.discriminator}`);
+        //'https://i.imgur.com/rNz0Wwj.png'
 
     var announceChannel = message.guild.channels.find(`name`, "mededelingen");
 

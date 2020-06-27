46488bf5f67ac2297ade61d11f1c342d42b1a833
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-06-27 18:58:08 +0200

    Update meld.js

--- Src/Javascripts/meld.js
+++ Src/Javascripts/meld.js
@@ -31,10 +31,9 @@ module.exports.run = async(bot, message, args) => {
     var announceEmbed = new discord.RichEmbed()
 
     .setColor("005fff")
-        .setAuthor('🔵 Mededeling Hometown Roleplay')
-        //.setThumbnail('https://i.imgur.com/rNz0Wwj.png')
-        .setDescription(`\n\n${announce}\n\n`)
-
+        .setAuthor('Mededeling Hometown Roleplay')
+        //.setDescription(`\n\n${announce}\n\n`)
+        .addField(':pauseblue::pauseblue::pauseblue::pauseblue::pauseblue::pauseblue::pauseblue::pauseblue::pauseblue::pauseblue:', '\n\n${announce}\n\n')
 
     //.setFooter('Hometown Roleplay V2 2020', );
     //.setFooter(`Mededeling van ${user.username}#${user.discriminator}`);

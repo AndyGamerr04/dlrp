a3cef758c27c035577dc9e6b82befa85d914fe86
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-11-05 20:02:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ bot.on("ready", async() => {
 
     //console.log(`${bot.user.username} is online!`);
     //bot.user.setActivity("BLACKPINK", { type: "listening" });
-    //bot.user.setActivity("Hometown Roleplay", { type: "playing" });
+    bot.user.setActivity("Hometown Roleplay", { type: "playing" });
     //bot.user.setPresence({ game: { name: 'Live streamen met', type: "streaming", url: "https://www.twitch.tv/relaxbeats" } });
 
 });

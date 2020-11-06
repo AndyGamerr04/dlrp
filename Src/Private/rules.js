967952573db3826616695632c9e4a5e7fb338746
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-11-06 13:01:12 +0100

    recode

--- Src/Private/rules.js
+++ Src/Private/rules.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args) => {
 
     message.delete();
 
-    //if (!message.member.roles.find(r => r.name === "Staff")) {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+    if (!message.member.roles.find(r => r.name === "Developer")) {
+        //if (!message.member.hasPermission("MANAGE_MESSAGES")) {
 
         var err = new discord.RichEmbed()
 
@@ -28,47 +28,6 @@ module.exports.run = async (bot, message, args) => {
         .setThumbnail('https://i.imgur.com/IBpr3Ow.png')
 
     message.channel.send(embed)
-
-    var embed2 = new discord.RichEmbed()
-
-        .setTitle('**RDM/VDM** - Vermoorden of aanvallen zonder roleplay.')
-
-        .setColor("#de3121")
-
-        .setDescription('**Geen RDM**\n(Random Death Match) Iemand zonder reden vermoorden.\n\n**Geen VDM**\n(Random Vehicle Deathmatch) Iemand dood rijden met een auto.\n\n**Geen SpawnKill**\nIemand vermoorden op het moment dat ze verbinden.\n\n**Geen FreeShot**\nIemand zonder reden schieten.\n\n**Zwaar wapen**\nGeen zwaar wapen Wapen uittrekken als iemand je gewapend in de gaten houdt.')
-
-    message.channel.send(embed2)
-
-
-    var embed3 = new discord.RichEmbed()
-
-        .setTitle('**RDM/VDM** - *Vermoorden of aanvallen zonder roleplay.*')
-
-        .setColor("#f59b14")
-
-        .setDescription('**Zwaar wapen**\nWapen uittrekken als iemand je gewapend in de gaten houdt.\n\n**Geen RDM**\n(Random Death Match) Iemand zonder reden vermoorden.\n\n**Geen VDM**\n(Random Vehicle Deathmatch) Iemand dood rijden met een auto.\n\n**Geen SpawnKill**\nIemand vermoorden op het moment dat ze verbinden.\n\n**Geen FreeShot**\nIemand zonder reden schieten.')
-
-    message.channel.send(embed3)
-
-
-    /*var embed4 = new discord.RichEmbed()
-
-        .setColor("#00ff11")
-
-        //.setAuthor('Het overtreden van regels leid tot staffzaak')
-
-        //.setTitle('HometownRP Reglement')
-
-        //.setURL('https://discord.gg/q8fB9QF')
-
-        .setThumbnail('https://i.imgur.com/IBpr3Ow.png')
-
-        .setDescription('**Zwaar wapen**\nWapen uittrekken als iemand je gewapend in de gaten houdt.\n\n**Geen RDM**\n(Random Death Match) Iemand zonder reden vermoorden.\n\n**Geen VDM**\n(Random Vehicle Deathmatch) Iemand dood rijden met een auto.\n\n**Geen SpawnKill**\nIemand vermoorden op het moment dat ze verbinden.\n\n**Geen FreeShot**\nIemand zonder reden schieten.')
-
-        .setFooter('Copyright HometownRP 2020 ©');
-
-    message.channel.send(embed4)*/
-
 
 }
 
--- index.js
+++ index.js
@@ -1,34 +1,12 @@
 const discord = require("discord.js");
-const botConfig = require("./botconfig.json");
+//const botConfig = require("./botconfig.json");
+const prefix = '!';
 const fs = require("fs");
 const bot = new discord.Client();
 bot.commands = new discord.Collection();
 
 fs.readdir("./Src/Javascripts/", (err, files) => {
 
-    if (err) console.log(err);
-
-    var jsFiles = files.filter(f => f.split(".").pop() === "js");
-
-    if (jsFiles.length <= 0) {
-
-        console.log("script not found!");
-        return;
-
-    }
-
-    jsFiles.forEach((f, i) => {
-
-        var fileGet = require(`./Src/Javascripts/${f}`);
-        bot.commands.set(fileGet.help.name, fileGet);
-
-    });
-
-});
-
-/*
-fs.readdir("./Commands/Errors/", (err, files) => {
-
   if (err) console.log(err);
 
   var jsFiles = files.filter(f => f.split(".").pop() === "js");
@@ -42,75 +20,59 @@ fs.readdir("./Commands/Errors/", (err, files) => {
 
   jsFiles.forEach((f, i) => {
 
-    var fileGet = require(`./Commands/Errors//${f}`);
+    var fileGet = require(`./Src/Javascripts/${f}`);
     bot.commands.set(fileGet.help.name, fileGet);
 
   });
 
 });
-*/
 
-/*
-fs.readdir("./Commands/Test/", (err, files) => {
+bot.on("ready", async () => {
 
-  if (err) console.log(err);
-
-  var jsFiles = files.filter(f => f.split(".").pop() === "js");
-
-  if (jsFiles.length <= 0) {
-
-    console.log("script not found!");
-    return;
-
-  }
-
-  jsFiles.forEach((f, i) => {
-
-    var fileGet = require(`./Commands/Test//${f}`);
-    bot.commands.set(fileGet.help.name, fileGet);
-
-  });
-
-});
-*/
-
-bot.on("ready", async() => {
-
-    //console.log(`${bot.user.username} is online!`);
-    //bot.user.setActivity("BLACKPINK", { type: "listening" });
-    bot.user.setActivity("Hometown Roleplay", { type: "playing" });
-    //bot.user.setPresence({ game: { name: 'Live streamen met', type: "streaming", url: "https://www.twitch.tv/relaxbeats" } });
+  //console.log(`${bot.user.username} is online!`);
+  //bot.user.setActivity("BLACKPINK", { type: "listening" });
+  bot.user.setActivity("Hometown Roleplay", { type: "playing" });
+  //bot.user.setPresence({ game: { name: 'Live streamen met', type: "streaming", url: "https://www.twitch.tv/relaxbeats" } });
 
 });
 
 bot.on('guildMemberAdd', member => {
 
-    var role = member.guild.roles.find('name', 'Member')
-    member.addRole(role)
+  var role = member.guild.roles.find('name', 'Member')
+  member.addRole(role)
 
 });
 
-bot.on("message", async message => {
+/*bot.on("message", async message => {
 
-    try {
+  //try {
 
-        if (message.author.bot) return;
+  if (message.author.bot) return;
 
-        if (message.channel.type === "dm") return;
+  if (message.channel.type === "dm") return;
 
-        var messageArrey = message.content.split(" ");
+  var messageArrey = message.content.split(" ");
 
-        var prefix = botConfig.prefix;
+  var prefix = botConfig.prefix;
 
-        var command = messageArrey[0];
+  var command = messageArrey[0];
 
-        var arguments = messageArrey.slice(1);
+  var arguments = messageArrey.slice(1);
 
-        var commands = bot.commands.get(command.slice(prefix.length));
+  var commands = bot.commands.get(command.slice(prefix.length));
 
-        if (command) commands.run(bot, message, arguments);
+  if (command) commands.run(bot, message, arguments);
 
-    } catch (err) {}
+  //} catch (err) { }
+
+});*/
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (command) commands.run(bot, message, arguments);
 
 });
 

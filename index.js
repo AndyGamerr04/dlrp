6648aa5ddf02e6652702bc4bd305978d8c0fb6bc
AndyGamerr04 <59649626+AndyGamerr04@users.noreply.github.com>
2020-11-06 13:04:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ bot.on('guildMemberAdd', member => {
 
 });*/
 
-client.on('message', message => {
+bot.on('message', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);

5d56717000eba0c91c75866e3a414dba865bfa57
Andyxdddd <59649626+Andyxdddd@users.noreply.github.com>
2020-12-03 21:23:40 +0100

    Update rules.js

--- Client/Javascripts/rules.js
+++ Client/Javascripts/rules.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
     var e1 = new discord.RichEmbed()
         .setColor("#93abd3")
         .setTitle('**Hometown Roleplay 2.0**')
-        .setDescription("*Zorg ervoor dat u de onderstaande regels leest.*\n\n**Waarschuwingen:\n> ● **2 waarschuwingen is 1 dag ban**.\n> ● **5 waarschuwingen is 1 week ban**.\n> ● **6 waarschuwingen is een permanente ban**.\n\n**LET OP!** Hacken of schelden met ziektes leidt tot een permanent ban.")
+        .setDescription("*Zorg ervoor dat u de onderstaande regels leest.*\n\n**Waarschuwingen:**\n> ● **2 waarschuwingen is 1 dag ban**.\n> ● **5 waarschuwingen is 1 week ban**.\n> ● **6 waarschuwingen is een permanente ban**.\n\n**LET OP!** Hacken of schelden met ziektes leidt tot een permanent ban.")
         .setImage('https://i.imgur.com/hmW0ZHX.png')
     message.channel.send(e1)
 
